7d3c80298e3fedd0b5f00d1dec1eb90e7dc52205
Alexander Moriarty <alexander@dal.ca>
2013-06-09 16:36:59 +0200

    test case

--- hw8/src/hw8_test.py
+++ hw8/src/hw8_test.py
@@ -7,17 +7,19 @@ import homework8.root_locus_extras as rlx
 
 def main():
     print "This is an example of homework8 root locus extras"
-    #s = sympy.Symbol("s")
-    #S = 1 / ((s+1)*(s+2)*(s+10))
+    s = sympy.Symbol("s")
+    S = 1 / ((s+1)*(s+2)*(s+10))
     
-    #numerS = map(float, sympy.Poly(S.as_numer_denom()[0], s).all_coeffs()) 
-    #denomS = map(float, sympy.Poly(S.as_numer_denom()[1], s).all_coeffs())
-    numerS = [1]
-    denomS = [1,3,2]
+    numerS = map(float, sympy.Poly(S.as_numer_denom()[0], s).all_coeffs()) 
+    denomS = map(float, sympy.Poly(S.as_numer_denom()[1], s).all_coeffs())
     tf = matlab.tf(numerS, denomS)
     
     gain = 0.11
     damping_ratio = 1.0
+    
+    print "provided test data :"
+    print "system =", tf, "\ngain =", gain,"\ndamping ratio =",damping_ratio
+    
     
     print "Gain ", rlx.gainFromDampingRatio(tf, damping_ratio)
     print "Poles: ", rlx.polesFromTransferFunction(tf)
@@ -27,7 +29,7 @@ def main():
     print "Frequency: ", rlx.frequencyFromGain(tf, gain)
     
     
-    """the damping factor, gain, pole locations, overshoot and frequency"""
+    """Things we were suppose to find: the damping factor, gain, pole locations, overshoot and frequency"""
     print rlx.__doc__
 
 if __name__=="__main__":
