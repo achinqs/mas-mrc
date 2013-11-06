b5e09d220b8ad87729bf6a2484ae9d48947f5998
Alexander Moriarty <alexander@dal.ca>
2013-11-06 17:23:13 +0100

    worked on A4. Finished
    Must create pdf and zip

--- notebooks/scratch.py
+++ notebooks/scratch.py
@@ -8,14 +8,21 @@ Created on Tue Oct 29 23:10:49 2013
 import numpy as np
 import numpy.linalg as LA
 
-import matplotlib as mpl
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 data = np.load("shape001.npy")
 
 x, y = data.T
 
+A = np.vstack([x, np.ones(len(x))]).T
+
+m, c = LA.lstsq(A, y)[0]
+
+print m, c
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.scatter(x,y)
+ax.plot(x, m*x + c, 'r', label='Fitted line')
+
+plt.show()
